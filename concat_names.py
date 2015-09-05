060653b315c7bca160c1510991f07fd7b0fc76c1
tuuli <tuuli.pollanen@gmail.com>
2015-09-04 23:07:31 -0700

    Add final remaining data frames, remove intermittent data frames, add fixed matches csv file

--- concat_names.py
+++ concat_names.py
@@ -16,26 +16,35 @@ def get_file_column(file):
 
 def populate_appointee_array(appointee_data):
   parsed_names = []
-  for name in appointee_data['CleanNameNoMiddle']:
+  for i, name in enumerate(appointee_data['CleanNameNoMiddle']):
     # Build an array decoding all of the appointee names and last names from utf-8 into unicode,
     # then convert those into ASCII ignoring errors, because the R output csv was converted into ASCII
-    parsed_names.append(("%s %s" % (HumanName(name.decode('utf-8')).first.lower(), 
-      HumanName(name.decode('utf-8')).last.lower())).encode('ascii', 'ignore'))
+    parsed_name = ("%s %s" % (HumanName(name.decode('utf-8')).first.lower(), 
+      HumanName(name.decode('utf-8')).last.lower())).encode('ascii', 'ignore')
+    parsed_names.append({
+      'organization': appointee_data['Organizations'][i],
+      'type': appointee_data['Type'][i],
+      'parsed_name': parsed_name,
+      'name' : appointee_data['CleanName'][i]
+    })
   return parsed_names
 
 def find_donor_appointees(donor_data, appointee_array):
-  donor_appointee_ids = []
-  for i, donor in enumerate(donor_data['name']):
-    if donor in appointee_array:
-      print "we have a match with %s" %(donor)
-      donor_appointee_ids.append(donor_data['contributor_id'][i])
-  return donor_appointee_ids
+  donor_appointees = []
+  for i, appointee in enumerate(appointee_array):
+    if appointee['parsed_name'] in donor_data['name']:
+      print "we have a match with %s" %(appointee['parsed_name'])
+      appointee['contributor_id'] = donor_data['contributor_id'][i]
+      donor_appointees.append(appointee)
+  return donor_appointees
 
 def write_results(res, csvfile):
+# "{'parsed_name': 'kathleen lyons', 'organization': 'Tax Court of Canada', 'contributor_id': '38789', 'type': 'Tax Court of Canada', 'name': 'Kathleen T. Lyons'}"
   with open(csvfile, "w") as output:
-      writer = csv.writer(output, lineterminator='\n')
-      for val in res:
-          writer.writerow([val]) 
+    writer = csv.writer(output, lineterminator='\n')
+    writer.writerow(res[0].keys())
+    for val in res:
+      writer.writerow(val.values()) 
 
 def run(appointees_file, donors_file):
   donor_data = get_file_column(donors_file)
